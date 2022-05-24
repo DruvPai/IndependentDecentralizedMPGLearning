54e215295ac394ba191408f5fdd9b5f58a3b349e
Druv Pai <druv@druvpai.com>
2022-05-24 00:14:02 -0700

    bookkeeping part 2

--- reproduce_figures.py
+++ reproduce_figures.py
@@ -54,10 +54,12 @@ def reproduce_figure_3():
     N = 10
     M = 2
     U = 2
+    m = [2, 4]
+    b = [9, 16]
     lambda_1 = 0.8
     lambda_2 = 0.2
     delta = 0.5
-    game, m, b = create_routing_game(N=N, M=M, U=U, lambda_1=lambda_1, lambda_2=lambda_2, delta=delta)
+    game, m, b = create_routing_game(N=N, M=M, U=U, m=m, b=b, lambda_1=lambda_1, lambda_2=lambda_2, delta=delta)
     K = int(1e5)
     for tau in [1e-3, 1e-6]:
         pi_history, q_tilde_history, s_history, a_history = independent_decentralized_algo(
